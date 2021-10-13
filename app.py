01dd003db9834df5b1bc29ced2492bc639fc2bcc
Ethan7102 <51076587+Ethan7102@users.noreply.github.com>
2021-10-14 01:08:12 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,105 @@
+from flask import Flask, jsonify
+from pymongo import MongoClient
+from bson.json_util import dumps
+from bson.json_util import loads
+import os
+import pymongo
+from urllib.parse import quote_plus
+
+# get mongodb's username, pwd, host name and port number from environment variables
+username = os.getenv('MONGO_USERNAME')
+password = os.getenv('MONGO_PASSWORD')
+host = os.getenv('MONGO_SERVER_HOST')
+port = os.getenv('MONGO_SERVER_PORT')
+
+# The flask API will print an error message and exit if the environment variables about MongoDB are not provided
+if username is None or password is None or host is None or port is None:
+    print('The environment variables about MongoDB are not provided.')
+    exit()
+
+# set url
+uri = "mongodb://%s:%s@%s:%s" % (quote_plus(username),
+                                 quote_plus(password), quote_plus(host), quote_plus(port))
+
+# connect mongodb
+try:
+    client = MongoClient(uri)
+    client.server_info()  # connect to mongoDB to get the server info
+# MongoDB will be accessed unsuccessfully if the environment variables about MongoDB are incorrect.
+except pymongo.errors.ServerSelectionTimeoutError:
+    print('Access failed. The environment variables about MongoDB are incorrect or the MongoDB server is not available.')
+    exit()
+
+# get the University DB
+db = client['university']
+students = db['student']
+takes = db['takes']
+
+app = Flask(__name__)
+
+# Return a JSON object with my Student ID and Name.
+
+
+@app.route('/me', methods=['GET'])
+def get_me():
+    # return my student information by JSON format
+    return jsonify({"student_id": "20035673D", "name": "Wong Ming Yuen"}), 200
+
+# Return a JSON object with all the students’ attributes
+
+
+@app.route('/students', methods=['GET'])
+def get_students():
+    # get all student data, disable _id coolumn and sort the students in ascending order of student_id
+    cursor = students.find({}, {'_id': 0}).sort('student_id', 1)
+    # iterate over to get a list of dicts
+    dicts = [doc for doc in cursor]
+    if len(dicts):  # if it is not empty
+        return jsonify(dicts), 200
+    else:  # print error message if no student records are found
+        return jsonify({'error': 'not found'}), 404
+
+# Return a JSON object with the specified student
+
+
+@app.route('/students/<student_id>', methods=['GET'])
+def get_student(student_id):
+    # get student data by student ID, disable _id coolumn
+    cursor = students.find({'student_id': {'$eq': student_id}}, {'_id': 0})
+    # iterate over to get a list of dicts
+    dicts = [doc for doc in cursor]
+    if len(dicts):  # if it is not empty
+        return jsonify(dicts), 200
+    else:  # print error message if /no student with the specified student ID is found
+        return jsonify({'error': 'not found'}), 404
+
+
+@app.route('/takes', methods=['GET'])
+def get_students_with_courses():
+    # aggregate two collection, the students were sorted in ascending order of student_id, the courses were sorted in ascending order of course_id
+    cursor = students.aggregate([{'$lookup': {'from': 'takes', 'let': {'student_id': '$student_id'}, 'pipeline': [{'$match': {'$expr': {'$and': [{'$eq': ['$student_id', '$$student_id']}]}}}, {
+                                '$project': {'_id': 0, 'student_id': 0}}, {'$sort': {'course_id': 1}}], 'as': 'student_takes'}}, {'$project': {'_id': 0}}, {'$sort': {'student_id': 1}}])
+    # iterate over to get a list of dicts
+    dicts = [doc for doc in cursor]
+    if len(dicts):  # if it is not empty
+        return jsonify(dicts), 200
+    else:  # print error message if no student records are found
+        return jsonify({'error': 'not found'}), 404
+
+
+@app.route('/takes/<student_id>', methods=['GET'])
+def get_student_with_courses(student_id):
+    # aggregate two collection, select the student by student_id,the students were sorted in ascending order of student_id, the courses were sorted in ascending order of course_id
+    cursor = students.aggregate([{'$match': {'student_id': student_id}}, {'$lookup': {'from': 'takes', 'let': {'student_id': '$student_id'}, 'pipeline': [{'$match': {'$expr': {'$and': [{'$eq': [
+                                '$student_id', '$$student_id']}]}}}, {'$project': {'_id': 0, 'student_id': 0}}, {'$sort': {'course_id': 1}}], 'as': 'student_takes'}}, {'$project': {'_id': 0}}, {'$sort': {'student_id': 1}}])
+    # iterate over to get a list of dicts
+    dicts = [doc for doc in cursor]
+    if len(dicts):  # if it is not empty
+        return jsonify(dicts), 200
+    else:  # print error message if no student record with the specified student ID is found
+        return jsonify({'error': 'not found'}), 404
+
+
+# start flask server
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=15000, debug=True)
---
+++ readme.md
@@ -0,0 +1,40 @@
+# Overview of the Python flask app
+This is a containerized python flask REST API using MongoDB as the backend database server. The python flask app communicates the MongoDB through a docker network ass1. It uses the various environment variables entered by the command 'docker run'  for passing the information about the MongoDB server’s hostname, port number, username and password. The app requires an available MongoDB server and the correct MongoDB server's information, otherwise, the app will fail, then print an error message and exit.
+The function of this app is to access students' data from the collection 'University ' in MongoDB, then present them in JSON format. It contains five endpoints as follows:
+
+*/me: Return a JSON object with my own Student ID and Name.
+```
+curl localhost:9990/me
+```
+
+*/students: Return a JSON object with all the students’ attributes
+```
+curl localhost:9990/students
+```
+
+*/students/<student_id>: Return a JSON object with the specified student 
+```
+curl localhost:9990/students/33333
+```
+
+*/takes: Return the attributes of all the students with the courses 
+```
+curl localhost:9990/takes
+```
+
+*/takes/<student_id>: Return a JSON object for the specified student
+```
+curl localhost:9990/takes/33333
+```
+
+For all of the endpoints above, the API will return an error message using JSON format with an HTTP response code of 404 if no matching student record is found.
+
+# Pull the image from the docker hub repository
+```
+docker pull polyu20035673d/student_svc
+```
+
+# Run the docker image
+```
+docker run --rm --network ass1 -e MONGO_USERNAME=comp3122 -e MONGO_PASSWORD=12345 -e MONGO_SERVER_HOST='mongo' -e MONGO_SERVER_PORT='27017' -p 9990:15000 polyu20035673d/student_svc
+```
